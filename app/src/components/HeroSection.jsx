1583080c60beb02776963af06f913182bda821bd
Armin Vejzovic <29armin.vejzovic@gmail.com>
2025-11-13 00:11:29 +0100

    deploy

--- app/src/components/HeroSection.jsx
+++ app/src/components/HeroSection.jsx
@@ -19,6 +19,8 @@ const HeroSection = () => {
   const link = document.createElement("a");
   link.href = fileUrl;
   link.download = fileName;
+  link.target = "_blank";
+  link.rel = "noopener noreferrer";
   link.click();
 
   window.open(fileUrl, "_blank");
